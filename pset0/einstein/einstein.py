cf21234082630f0682aa5347ba04d8453ffa41f6
Igor Azovskiy <118483800+GAPLU@users.noreply.github.com>
2023-02-21 13:07:12 +0100

    Added problem set 0

---
+++ pset0/einstein/einstein.py
@@ -0,0 +1,5 @@
+def main():
+    m = int(input("m: "))
+    print("E:", m * 300000000 * 300000000)
+
+main()
---
+++ pset0/tip/tip.py
@@ -0,0 +1,18 @@
+def main():
+    dollars = dollars_to_float(input("How much was the meal? "))
+    percent = percent_to_float(input("What percentage would you like to tip? "))
+    tip = dollars * percent
+    print(f"Leave ${tip:.2f}")
+
+
+def dollars_to_float(d):
+    # convert str into float
+    d = d.lstrip("$")
+    return float(d)
+
+def percent_to_float(p):
+    # convert percents to rate
+    p = p.rstrip("%")
+    return float(p) / 100
+
+main()
